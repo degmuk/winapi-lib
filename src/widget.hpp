c55403499950a61b662d46e2c18b4a9783d23ea6
dgmk <degmuk@gmail.com>
2021-09-29 10:28:34 +0300

    ListBox

--- src/widget.hpp
+++ src/widget.hpp
@@ -4,6 +4,7 @@
 #include "src/basic_widget.hpp"
 #include "src/button.hpp"
 #include "src/label.hpp"
+#include "src/listbox.hpp"
 
 class Widget : public BasicWidget {
  public:
@@ -13,11 +14,13 @@ class Widget : public BasicWidget {
   }
 
  private:
-  Button button_ = Button(this, 100, 100, L"Юникод", []() {
+  Button button_ = Button(this, 0, 20, L"Юникод", []() {
     MessageBoxW(0, L"Текст", L"Заголовок", MB_ICONINFORMATION | MB_OK);
   });
 
   Label label_ = Label(this, 0, 0, L"Fuck ты");
+
+  ListBox listbox_ = ListBox(this, 100, 0, 200, 200);
 };
 
 #endif  // WIDGET_HPP_
